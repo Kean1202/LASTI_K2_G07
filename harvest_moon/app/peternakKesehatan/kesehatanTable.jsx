4b55cc48c80fb003de37cc7e4b018dd64806fee6
Muhammad Rafi Haidar <93863041+haidar2003@users.noreply.github.com>
2023-12-09 13:31:31 +0700

    feat: hewan

--- harvest_moon/app/peternakKesehatan/kesehatanTable.jsx
+++ harvest_moon/app/peternakKesehatan/kesehatanTable.jsx
@@ -6,6 +6,54 @@ import ButtonInTable from "@/components/ButtonInTable";
 
 
 const KesehatanTable = () => {
+    const [data, setData] = useState(props.dataHewan);
+
+    function action(id){
+        setData(prevData => prevData.map(row => {
+            if (row.id === id) {
+                if (row.kondisi_hewan == 'Sick'){
+                    return {
+                        ...row,
+                        kondisi_hewan: 'Healthy'
+                      };
+                } else{
+                    return row;  
+                }
+            } else {
+              return row;
+            }
+          }));
+        console.log(data)
+    }
+
+    const ButtonInTable = ({buttonText, size, popupMessage, action_func, id}) => {
+        let buttonSize = "h-12 w-28"; // Default size
+        if (size == "small") {
+          buttonSize = "h-8 w-20"; // Small size
+        } else if (size == "large") {
+          buttonSize = "h-16 w-36";
+        }
+      
+        const [showPopup, setShowPopup] = useState(false);
+      
+        const handleClick = () => {
+          action_func(id); 
+          setShowPopup(true);
+          setTimeout(() => {
+            setShowPopup(false);
+          }, 3000);
+        };
+      
+        return (
+          <div className="mx-2 my-8 rounded-3xl border border-black button_bg ${buttonSize} transition-all font-inter
+          hover:bg-yellow hover:text-black text-center text-xl flex items-center justify-center">
+              <button onClick={handleClick}>{buttonText}</button>
+              {showPopup && <Popup message={popupMessage} />}
+          </div>
+          
+        )
+      }
+
     // Table columns
 	const columns = [
 		{
@@ -27,55 +75,32 @@ const KesehatanTable = () => {
         
 	];
 
-    const rows = [
-        {
-            key: "1",
-            ID_hewan: "001",
-            jenis_hewan: "Sapi",
-            kondisi_hewan: "Healthy",
-            action_hewan: 
-            <ButtonInTable
-                buttonText={"Obati"}
-                size="small"
-                popupMessage={"Peternak Melakukan Pengobatan"}
-            />
-        },
-
-        {
-            key: "2",
-            ID_hewan: "002",
-            jenis_hewan: "Sapi",
-            kondisi_hewan: "Sick",
-            action_hewan: 
-            <ButtonInTable
-                buttonText={"Obati"}
-                size="small"
-                popupMessage={"Peternak Melakukan Pengobatan"}
-            />
-        },
-
-        {
-            key: "3",
-            ID_hewan: "003",
-            jenis_hewan: "Ayam",
-            kondisi_hewan: "Healthy",
-            action_hewan: 
-            <ButtonInTable
-                buttonText={"Obati"}
-                size="small"
-                popupMessage={"Peternak Melakukan Pengobatan"}
-            />
-        },
-    ]
+    function mapRows(data){
+        return data.map((currData) => {
+            return {
+                key: currData.id,
+                jenis_hewan: currData.jenis_hewan,
+                kondisi_hewan: currData.kondisi_hewan,
+                action_tanaman: 
+                <ButtonInTable
+                    buttonText={"Obati"}
+                    size="small"
+                    popupMessage={`Perintah telah dikirim ke petani dengan ID hewan ${currData.id}`}
+                    action_func={action}
+                    id={currData.id}
+                />
+            }
+        })
+    }
 
 	// table rows
   
     return (
         <Table aria-label="Tabel Pertumbuhan Tanaman" align="center" shadow="md" isStriped>
             <TableHeader columns={columns}>
-                    {(column) => <TableColumn key={column.key}  className="green_gradient font-bold">{column.label}</TableColumn>}
+                    {(column) => <TableColumn key={column.key}  className="green_gradient font-bold" style={{backgroundColor: '#b0dcbc'}}>{column.label}</TableColumn>}
             </TableHeader>
-            <TableBody items={rows}>
+            <TableBody items={mapRows(data)}>
                 {(item) => (
                     <TableRow key={item.key} align="center">
                             {(columnKey) => <TableCell className="font-inter text-center">{getKeyValue(item, columnKey)}</TableCell>}
